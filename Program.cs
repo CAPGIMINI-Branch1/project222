363e94c88b78603da4e1e7f9db66cbade6e7bb8c
Ravulaprathimareddy <prathimareddyravula@gmail.com>
2022-02-12 17:20:41 +0530

    added new dotnet console

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace deva
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
